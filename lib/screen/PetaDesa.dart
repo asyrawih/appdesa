48f1d37b5824a064db10cf703b66c4adadb2ac16
= <=>
2020-03-02 21:28:04 +0800

    SomeFix

--- lib/screen/PetaDesa.dart
+++ lib/screen/PetaDesa.dart
@@ -1,6 +1,7 @@
 import 'package:appdesa/style.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PetaDesa extends StatelessWidget {
   @override
@@ -22,9 +23,12 @@ class PetaDesa extends StatelessWidget {
           ],
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[],
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(3.595196, 98.672226),
+          zoom: 14.0,
+        ),
       ),
     );
   }
